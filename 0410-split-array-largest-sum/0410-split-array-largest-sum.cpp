3b9a1dc5ecb41b0388d2640159a8ade0cb0819a2
AyushSinha229 <131798005+AyushSinha229@users.noreply.github.com>
2026-08-05 21:54:23 +0530

    Time: 0 ms (100%), Space: 10.6 MB (43.65%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
 
-    bool ispossible(vector<int>& arr,int maxSum, int k){
-        int pages = 0, allocated = 1;
+    bool possible(vector<int>& arr, int m , int k){
 
-        for(int i = 0 ; i < arr.size(); i++){
+        int count = 1, pages = 0;
 
-            if(arr[i] > maxSum) return false;
-
-            if(arr[i] + pages > maxSum){
-                allocated += 1;
+        for(int i = 0 ; i < arr.size() ; i++){
+            if(pages + arr[i] > m){
+                count++;
                 pages = arr[i];
             }else pages += arr[i];
         }
-        return allocated <= k;
+
+        return count <= k;
     }
     int splitArray(vector<int>& arr, int k) {
 
         int low = *max_element(arr.begin(), arr.end());
         int high = 0;
-
-        for (int x : arr)
+        for(auto x : arr){
             high += x;
-
-        int ans = high;
-
+        }
+        int ans =high;
         while(low <= high){
+            int mid = low +(high - low)/2;
 
-            int mid = low + (high - low)/ 2;
-
-            if(ispossible(arr, mid , k)){
-                ans = mid;
+            if(possible(arr,mid,k)){
+                ans = mid; 
                 high = mid - 1;
             }else low = mid + 1;
         }
 
         return ans;
-        
+
     }
 };

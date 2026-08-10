8aac3cd0f3f8320d6347bc3a62012c48b70aed18
AyushSinha229 <131798005+AyushSinha229@users.noreply.github.com>
2026-08-10 21:18:54 +0530

    Time: 19 ms (5.25%), Space: 13.7 MB (7.13%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+
+        map<ListNode*, int> mpp;
+
+        ListNode* temp = head;
+
+        while(temp != nullptr){
+
+            if(mpp.find(temp) != mpp.end()) return temp;
+
+            mpp[temp] = 1;
+
+            temp = temp->next;
+        }
+
+        return nullptr;
+        
+    }
+};

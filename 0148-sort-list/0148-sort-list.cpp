7823ecac43182462f8e18993bef700c866b2cc9d
AyushSinha229 <131798005+AyushSinha229@users.noreply.github.com>
2026-08-13 21:19:17 +0530

    Time: 12 ms (73.71%), Space: 56.8 MB (94.68%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -1,39 +1,50 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
+    ListNode* merge(ListNode* left, ListNode* right) {
+
+        ListNode dummy(0);
+        ListNode* temp = &dummy;
+
+        while (left && right) {
+
+            if (left->val <= right->val) {
+                temp->next = left;
+                left = left->next;
+            } else {
+                temp->next = right;
+                right = right->next;
+            }
+
+            temp = temp->next;
+        }
+
+        if (left)
+            temp->next = left;
+
+        if (right)
+            temp->next = right;
+
+        return dummy.next;
+    }
     ListNode* sortList(ListNode* head) {
-        ListNode* temp = head;
-        int n = 0;
 
-        while(temp != nullptr){
-            temp = temp->next;
-            n++;
-        }
-        temp = head;
+        if (head == nullptr || head->next == nullptr)
+            return head;
 
-        vector<int> arr(n);
+        ListNode* slow = head;
+        ListNode* fast = head->next;
 
-        for(int i = 0 ; i < n ; i++){
-            arr[i] = temp->val;
-            temp = temp->next;
-        }
-        sort(arr.begin(), arr.end());
-        temp = head;
-        for(int i = 0 ; i < n ; i++){
-            temp->val = arr[i];
-            temp = temp->next;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        return head;
+        ListNode* right = slow->next;
+        slow->next = nullptr;
 
+        ListNode* left = sortList(head);
+        right = sortList(right);
+
+        return merge(left, right);
     }
 };

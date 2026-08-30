37aaeec3bfdad748378208e0f637892c80560b87
AyushSinha229 <131798005+AyushSinha229@users.noreply.github.com>
2026-08-30 21:28:30 +0530

    Time: 0 ms (100%), Space: 11.4 MB (19.23%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -7,7 +7,7 @@ public:
 
         for(int i = 0; i < n ; i++){
 
-            if (arr[i] > arr[(i + 1) % n]){
+            if (arr[i] > arr[(i+1) % n]){
                 count++;
             }
         }

b74a0d9c9839807961e63bc55813f4e968fb0dbd
AyushSinha229 <131798005+AyushSinha229@users.noreply.github.com>
2026-08-30 22:04:54 +0530

    Time: 0 ms (100%), Space: 24 MB (7.86%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& arr) {
-        int j = 0;
+        int n = arr.size();
 
-        for(int i = 0 ; i < arr.size() ; i++){
+        int j = 0;
+        for(int i = 0; i < n; i++){
             if(arr[i] != 0){
                 swap(arr[i], arr[j]);
                 j++;

e587b27ad375d0e3a98689c966af239ab00e9e5d
AyushSinha229 <131798005+AyushSinha229@users.noreply.github.com>
2026-09-21 20:54:36 +0530

    Time: 0 ms (100%), Space: 13.6 MB (43.29%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -6,13 +6,11 @@ public:
         int low = 0 , high = n -1;
 
         while(low <= high){
-
             int mid = low + (high - low)/2;
 
             if(arr[mid] == target) return mid;
             else if(arr[mid] > target) high = mid - 1;
             else low = mid + 1;
-
         }
 
         return low;

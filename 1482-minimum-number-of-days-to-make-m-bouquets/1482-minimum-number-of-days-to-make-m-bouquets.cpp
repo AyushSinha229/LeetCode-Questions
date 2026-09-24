3c96b9015c922168ca3ec7161f84d0450231d4ab
AyushSinha229 <131798005+AyushSinha229@users.noreply.github.com>
2026-09-24 22:01:46 +0530

    Time: 20 ms (68.49%), Space: 70.7 MB (12.76%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
 
-    bool possible(vector<int> & arr, int day, int m , int k){
-        int count = 0, bloom = 0;
+    bool possible(vector<int>& arr, int day , int m, int k){
+        int count = 0 , bloom = 0;
 
-        for(int i = 0 ; i < arr.size(); i++){
-
-            if(arr[i] <= day) count++;
-            else {
-                bloom += count / k;
-                count = 0;
+        for(auto x : arr){
+            if(x <= day) count++;
+            else{
+                bloom += count/k;
+                count = 0;;
             }
         }
         bloom += count / k;
@@ -18,28 +17,27 @@ public:
         else return false;
 
     }
-    
     int minDays(vector<int>& arr, int m, int k) {
 
         int mini = INT_MAX;
         int maxi = INT_MIN;
-        for(int i = 0 ; i < arr.size() ; i++){
-            mini = min(mini, arr[i]);
-            maxi = max(maxi, arr[i]);
-        }
-        if((long long)m * k > arr.size())
-        return -1;
 
+        for(auto x : arr){
+            mini = min(mini,x);
+            maxi = max(maxi,x);
+        }
         int low = mini, high = maxi, ans = -1;
 
         while(low <= high){
-            int mid = low + (high - low) / 2;
+            int mid = low + (high - low)/2;
+
             if(possible(arr, mid , m , k) == true){
                 ans = mid;
                 high = mid - 1;
                 
             }else low = mid + 1;
         }
-        return ans;   
+
+        return ans;
     }
 };
